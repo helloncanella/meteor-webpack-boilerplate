5cde63dd8c781c068e9a48baaa15c153cce70cea
Hellon Canella <helloncanella@gmail.com>
2016-08-16 01:36:20 -0300

    solved old problem

--- src/HelloApp/client/HelloApp.jsx
+++
@@ -1,9 +0,0 @@
-// src/HelloApp/client/HelloApp.jsx
-import React, { Component } from 'react';
-import Hello from './Hello';
-
-export default class HelloApp extends Component {
-  render() {
-    return <Hello name="ishi" />;
-  }
-}
