54620046a238f94be7581a2e61024ff98a81646b
Hellon Canella <helloncanella@gmail.com>
2016-08-16 01:13:49 -0300

    removing react

---
+++ src/client.ts
@@ -0,0 +1,20 @@
+// src/client.jsx
+// import ReactDOM from 'react-dom';
+// import HelloApp from 'HelloApp/client/HelloApp';
+// import React from 'react';
+// import oi from 'package-test/test';
+// // import Meteor from 'meteor/meteor';
+
+// // Meteor.startup(() => {
+
+  
+// //   // ReactDOM.render(
+// //   //   <HelloApp />,
+// //   //   document.getElementById('react-app')
+// //   // );
+// // });
+
+
+// oi();
+
+
